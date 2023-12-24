b678d90a886315d925361acd0b3bcf14a5dee03b
최은석 <trgf456@naver.com>
2023-12-24 19:35:16 +0900

    [init] login layout

--- src/app/(notuser)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(notuser)/@modal/(.)i/flow/login/page.tsx
@@ -1,9 +1,16 @@
+import SignUpMain from "@/components/auth/signup";
 import LoginModal from "@/components/flow/LoginModal";
+import Modal from "@/components/ui/modal";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <>
-      <LoginModal />
+      <Modal height="650" width="600">
+        <Suspense fallback={<>loading...</>}>
+          <SignUpMain />
+        </Suspense>
+      </Modal>
     </>
   );
 }
