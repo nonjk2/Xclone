322bd012e74e49ebb6a569cea13ec62619714f17
최은석 <trgf456@naver.com>
2024-02-28 19:08:44 +0900

    [init]이미지 구간 분류

---
+++ src/components/main/center/home/HomePostImage.tsx
@@ -0,0 +1,124 @@
+"use client";
+import Image from "next/image";
+import { faker } from "@faker-js/faker";
+import { useRouter } from "next/navigation";
+import { MouseEvent, MouseEventHandler } from "react";
+
+const HomePostImage = ({
+  Images,
+  id,
+  postId,
+}: {
+  Images: PostImage[];
+  id: string;
+  postId: number;
+}) => {
+  const { push } = useRouter();
+
+  const onClickImageRouteHandler = (
+    e: MouseEvent<HTMLDivElement>,
+    ImageId: number
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
+    push(`/${id}/status/${postId}/photo/${ImageId}`, { scroll: false });
+  };
+  switch (Images.length) {
+    case 1:
+      return (
+        <div
+          className="w-full h-full absolute inset-0"
+          onClick={(e) => onClickImageRouteHandler(e, 1)}
+        >
+          <Image
+            src={`${Images[0].link}`}
+            alt="Image description"
+            className="w-full h-full"
+            width={100}
+            height={100}
+          />
+        </div>
+      );
+    case 2:
+      return (
+        <div className="flex divide-x-2 w-full h-full absolute top-0 divide-white">
+          {Images.map((img, idx) => (
+            <div
+              key={img.imageId}
+              className="w-1/2 h-full relative"
+              onClick={(e) => onClickImageRouteHandler(e, idx + 1)}
+            >
+              <Image
+                src={`${Images[idx].link}`}
+                alt="Image description"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className="object-cover"
+                fill
+              />
+            </div>
+          ))}
+        </div>
+      );
+    case 3:
+      return (
+        <div className="flex divide-x-2 w-full h-full absolute top-0 divide-white">
+          <div
+            className="w-1/2 h-full relative"
+            onClick={(e) => onClickImageRouteHandler(e, 1)}
+          >
+            <Image
+              src={`${Images[0].link}`}
+              alt="Image description"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-cover"
+              fill
+            />
+          </div>
+          <div className="w-1/2 h-full relative">
+            <div className="flex flex-col w-full h-full divide-y-2 divide-white absolute inset-0">
+              {Images.slice(1).map((img, idx) => (
+                <div
+                  className="w-full h-1/2 relative"
+                  key={img.imageId + idx}
+                  onClick={(e) => onClickImageRouteHandler(e, idx + 2)}
+                >
+                  <Image
+                    src={`${Images[idx + 1].link}`}
+                    alt="Image description"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    case 4:
+      return (
+        <div className="flex divide-x-2 divide-y-2 w-full h-full absolute top-0 divide-white flex-wrap">
+          {Images.map((img, idx) => (
+            <div
+              key={img.imageId}
+              className="w-1/2 h-1/2 relative"
+              onClick={(e) => onClickImageRouteHandler(e, idx + 1)}
+            >
+              <Image
+                src={`${Images[idx].link}`}
+                alt="Image description"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                fill
+                className="object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      );
+
+    default:
+      return <></>;
+  }
+};
+
+export default HomePostImage;
