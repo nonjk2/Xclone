e3d73874cdaa6ca7f4a1468b242bc4a56a9326e6
최은석 <trgf456@naver.com>
2024-03-03 20:31:22 +0900

    [init] onsubmit 달기

--- src/components/auth/signin/signInSecond.tsx
+++ src/components/auth/signin/signInSecond.tsx
@@ -13,46 +13,47 @@ const SignInSecond = () => {
   };
   return (
     <>
-      <div className="w-full h-[453px] m-auto px-20">
-        <div className="flex items-center w-full h-[76px]">
-          <h1 className="font-bold text-[31px] text-blackText">
-            <span>비밀번호를 입력하세요</span>
-          </h1>
-        </div>
+      <form action="">
+        <div className="w-full h-[453px] m-auto px-20">
+          <div className="flex items-center w-full h-[76px]">
+            <h1 className="font-bold text-[31px] text-blackText">
+              <span>비밀번호를 입력하세요</span>
+            </h1>
+          </div>
 
-        {/* Input */}
-        <DynamicInput
-          placeholder="이메일"
-          id="login"
-          type="email"
-          disabled
-          value={loginInputValue["login"]}
-        />
-        <DynamicInput
-          placeholder="비밀번호"
-          id="password"
-          type="password"
-          handleInputChange={signInOnChangeHandler}
-          value={loginInputValue["password"]}
-        />
-      </div>
-      <div className="w-full h-[144px] px-20">
-        <div className="flex w-full h-[100px] my-6">
-          <Button
-            backgroundColor="black"
-            color="white"
-            hoverColor="hoverBlack"
-            size="register"
-            // onClick={nextStep}
-            //   onClick={nextStep}
-            title={
-              <div className="flex justify-center text-[17px] font-bold leading-5">
-                <span>로그인하기</span>
-              </div>
-            }
+          <DynamicInput
+            placeholder="이메일"
+            id="login"
+            type="email"
+            disabled
+            value={loginInputValue["login"]}
+          />
+          <DynamicInput
+            placeholder="비밀번호"
+            id="password"
+            type="password"
+            handleInputChange={signInOnChangeHandler}
+            value={loginInputValue["password"]}
           />
         </div>
-      </div>
+        <div className="w-full h-[144px] px-20">
+          <div className="flex w-full h-[100px] my-6">
+            <Button
+              backgroundColor="black"
+              color="white"
+              hoverColor="hoverBlack"
+              size="register"
+              // onClick={nextStep}
+              //   onClick={nextStep}
+              title={
+                <div className="flex justify-center text-[17px] font-bold leading-5">
+                  <span>로그인하기</span>
+                </div>
+              }
+            />
+          </div>
+        </div>
+      </form>
     </>
   );
 };
