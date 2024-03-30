4aa8be33237fa78d8e95c1238f8094ef20b37234
최은석 <trgf456@naver.com>
2024-03-30 20:41:19 +0900

    [feat] 로그인 진입 로직 추가

--- src/app/(notuser)/page.tsx
+++ src/app/(notuser)/page.tsx
@@ -1,7 +1,14 @@
+import { authOption } from "@/auth";
 import Main from "@/components/auth/main";
 import Footer from "@/components/flow/footer";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const session = await getServerSession(authOption);
+  if (session?.user) {
+    return redirect("/home");
+  }
   return (
     <>
       <Main />
