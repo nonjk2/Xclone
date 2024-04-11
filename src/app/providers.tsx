687814bb54be375f77cfaca2e534aa567a7a7992
최은석 <trgf456@naver.com>
2024-04-11 18:49:45 +0900

    [feat] authprovider 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,21 @@
+"use client";
+import AuthProvider from "@/context/AuthProvider";
 import QueryProvider from "@/context/QueryProvider";
 import RecoilProvider from "@/context/RecoilProvider";
 import StyleProvider from "@/context/StyleProvider";
+import { SessionProvider } from "next-auth/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <QueryProvider>
-      <RecoilProvider>
-        <StyleProvider>{children}</StyleProvider>
-      </RecoilProvider>
-    </QueryProvider>
+    <SessionProvider>
+      <AuthProvider>
+        <QueryProvider>
+          <RecoilProvider>
+            <StyleProvider>{children}</StyleProvider>
+          </RecoilProvider>
+        </QueryProvider>
+      </AuthProvider>
+    </SessionProvider>
   );
 };
 export default Providers;
