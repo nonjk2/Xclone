30c063ffcd0962523fb2821425e209bf2afaf6f7
최은석 <trgf456@naver.com>
2024-04-17 18:38:38 +0900

    [feat] token으로 유저 가져오기 로직 분리

--- src/lib/action/auth-server.ts
+++ src/lib/action/auth-server.ts
@@ -1,8 +1,9 @@
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 const url = `${supabaseURL}/rest/v1`;
-const fetchUserFromDatabase = async (userId: string): Promise<authUser[]> => {
+const fetchUserFromDatabase = async (userId: string): Promise<any> => {
   const query = `userinfo?select=*&user_id=eq.${userId}`;
+
   try {
     const res = await fetch(`${url}/${query}`, {
       method: "GET",
@@ -16,6 +17,7 @@ const fetchUserFromDatabase = async (userId: string): Promise<authUser[]> => {
     if (!res.ok) {
       throw new Error("failed", { cause: res.statusText });
     }
+
     return res.json();
   } catch (error) {
     throw new Error(error as string);
