3af3934d43c9b892563f6282741033ffca373255
최은석 <trgf456@naver.com>
2024-04-17 18:38:25 +0900

    [feat] queryprovider 자리 이동

--- src/context/QueryProvider.tsx
+++ src/context/QueryProvider.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
 import { ReactNode, useState } from "react";
 
 interface QueryProviderProps {
@@ -21,10 +22,13 @@ const QueryProvider = ({ children }: QueryProviderProps) => {
   );
 
   return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={client}>
+        {children}
+
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </SessionProvider>
   );
 };
 
