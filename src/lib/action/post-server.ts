489bcf965fd15235b876f88c4ded17d89f6f4a9f
최은석 <trgf456@naver.com>
2024-04-17 18:39:44 +0900

    [feat] post 게시물 가져오기 및 insert추가로직 fetch로직 등
    2. supabaseClient 싱글톤패턴으로  객체 가져오기 추가
    3. 좋아요 update 로직 추가

--- src/lib/action/post-server.ts
+++ src/lib/action/post-server.ts
@@ -1,11 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
 import { v4 as uuidv4 } from "uuid";
-import { Database } from "../../../database.types";
-
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
-);
+import { supabaseClient } from "./\bsupabase";
 
 interface FetchPostInterface {
   content: string;
@@ -27,30 +21,98 @@ interface PostPathInterface {
  * postlist 가져오기 home
  *
  */
+
+interface SinglePostPropsType {
+  queryKey: [string, string];
+
+  supabaseAccessToken: string;
+}
+const getSinglePost = async ({
+  queryKey,
+  supabaseAccessToken,
+}: SinglePostPropsType): Promise<Post> => {
+  const [_, id] = queryKey;
+  try {
+    const supabase = supabaseClient(supabaseAccessToken);
+    const { data: user, error: userinfoError } = await supabase
+      .from("userinfo")
+      .select("id")
+      .single();
+
+    if (!user) {
+      throw new Error("user not found");
+    }
+    const { data: newPost, error } = await supabase
+      .from("posts")
+      .select(
+        `* ,
+        User:userinfo!public_posts_user_id_fkey(*),
+        Images:post_images(id,link),
+        Heart:post_likes(user_id)
+      `
+      )
+      .eq("id", id)
+      .single();
+    const heartLikedByUser =
+      (newPost.Heart as Heart) &&
+      newPost.Heart.find((e: Heart) => e.user_id === user.id);
+    const post: Post = {
+      HeartLiked: !!heartLikedByUser,
+      id: newPost.id,
+      User: newPost.User,
+      content: newPost.content,
+      createdAt: newPost.created_at,
+      Images: newPost.Images,
+      Heart: newPost.Heart,
+      _count: { Hearts: newPost.Heart.length, Reposts: 0, Comments: 0 },
+      OriginalPost: newPost.is_original,
+    };
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    return post;
+  } catch (error) {
+    throw new Error("error");
+  }
+};
+
 const updateHeartPost = async ({
-  user_id,
+  supabaseAccessToken,
   post_id,
   type,
-  heart_id,
 }: {
-  user_id: string;
+  supabaseAccessToken: string;
   post_id: string;
   type: "unlike" | "like";
   heart_id?: number;
 }) => {
+  const supabase = supabaseClient(supabaseAccessToken);
+  const { data: user, error: userinfoError } = await supabase
+    .from("users")
+    .select("id")
+    .single();
+
+  if (userinfoError) {
+    throw new Error("user not found");
+  }
+
+  const user_id = user.id;
   try {
     if (type === "like") {
       const { data, error } = await supabase
         .from("post_likes")
         .insert({ user_id, post_id })
         .select();
-      if (error) throw new Error(error.message);
+      if (error) {
+        throw new Error(error.message);
+      }
       return data;
-    } else {
+    } else if (type === "unlike") {
       const { data, error } = await supabase
         .from("post_likes")
         .delete()
-        .eq("id", heart_id)
+        .match({ user_id, post_id })
         .select();
       if (error) {
         throw new Error(error.message);
@@ -62,42 +124,65 @@ const updateHeartPost = async ({
   }
 };
 
+interface getPostListType {
+  pageParam: string | undefined;
+  supabaseAccessToken: string;
+}
+
 const getPostList = async ({
   pageParam = undefined,
-}: {
-  pageParam: string | undefined;
-}): Promise<Post[]> => {
+  supabaseAccessToken,
+}: getPostListType): Promise<Post[]> => {
+  const supabase = supabaseClient(supabaseAccessToken);
+  // const supabase = supabaseClientNotAuth();
+  // console.log(supabaseAccessToken);
   try {
+    const { data: user, error: userinfoError } = await supabase
+      .from("users")
+      .select("id")
+      .single();
+    // .single();
+    console.log(user);
+    if (userinfoError) {
+      console.log(userinfoError);
+    }
+    if (!user) {
+      throw new Error("User not found");
+    }
     let query = supabase
       .from("posts")
       .select(
         `* ,
-        User:userinfo!public_posts_user_id_fkey(*),
+        User:userinfo(*),
         Images:post_images(id,link),
-        Heart:post_likes(*)
+        Heart:post_likes(user_id)
       `
       )
       .order("created_at", { ascending: false })
       .limit(5);
-
     if (pageParam) {
       query = query.lt("created_at", pageParam);
     }
 
     const { data: newPost, error } = await query;
+    console.log("newPost : ", newPost);
     if (error) {
       throw new Error(error.message);
     }
-    console.log(newPost);
+
     const post: Post[] = newPost.map((e) => {
+      const heartLikedByUser =
+        (e.Heart as Heart[]) &&
+        e.Heart.find((e: Heart) => e.user_id === user.id);
       return {
+        HeartLiked: !!heartLikedByUser?.user_id,
         id: e.id,
-        User: e.User,
+        User: e.User as authUser,
         content: e.content,
         createdAt: e.created_at,
         Images: e.Images,
         Heart: e.Heart,
-        _count: { Hearts: 0, Reposts: 0, Comments: 0 },
+        _count: { Hearts: e.Heart.length, Reposts: 0, Comments: 0 },
         OriginalPost: e.is_original,
         // Parent: e.parent_post_id ?? "",
         // ParentPost: !!e.parent_post_id,
@@ -112,19 +197,36 @@ const getPostList = async ({
 
 // 새로운 Post를 생성하는 비동기 함수
 const createPost = async (formdata: {
-  userId: string;
+  supabaseAccessToken: string;
   content: string;
   isOriginal: boolean;
   images?: FileList | null;
   parentPostId?: string;
 }): Promise<Post> => {
-  const { content, isOriginal, userId, images, parentPostId } = formdata;
+  const { content, isOriginal, supabaseAccessToken, images, parentPostId } =
+    formdata;
   try {
+    const supabase = supabaseClient(supabaseAccessToken);
+    // const supabase = supabaseClientNotAuth();
+    console.log(supabaseAccessToken);
+    const { data: user, error: userinfoError } = await supabase
+      .from("users")
+      .select("*")
+      .single();
+
+    console.log(user);
+    if (userinfoError) {
+      console.log(userinfoError);
+    }
+    if (!user) {
+      throw new Error("user not found");
+    }
+
     const { data, error } = await supabase
       .from("posts")
       .insert([
         {
-          user_id: userId,
+          user_id: user.id,
           content,
           is_original: isOriginal,
           parent_post_id: parentPostId,
@@ -143,7 +245,7 @@ const createPost = async (formdata: {
     }
     let imageUrl = {} as PostImage;
     if (images && images.length > 0) {
-      imageUrl = await insertImage(images[0], userId, data.id);
+      imageUrl = await insertImage(images[0], user.id, data.id);
     }
 
     const newPost = { ...data } as unknown as FetchPostInterface;
@@ -173,6 +275,7 @@ const insertImage = async (
   post_id: string
 ): Promise<PostImage> => {
   try {
+    const supabase = supabaseClient();
     // 스토리지 저장 !!
     const { data: uploadedFile, error: storageError } = await supabase.storage
       .from("posts")
@@ -206,4 +309,4 @@ const insertImage = async (
   }
 };
 
-export { createPost, getPostList, updateHeartPost };
+export { createPost, getPostList, updateHeartPost, getSinglePost };
