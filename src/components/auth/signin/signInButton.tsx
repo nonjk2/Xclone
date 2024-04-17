ec1f9facc2a7d642e52ab933aba18dcdcb50e05e
최은석 <trgf456@naver.com>
2024-04-17 18:34:31 +0900

    [feat] 카카오 로긴 ui 연결

--- src/components/auth/signin/signInButton.tsx
+++ src/components/auth/signin/signInButton.tsx
@@ -1,15 +1,38 @@
 "use client";
 import Button from "@/components/ui/button";
-import { GoogleIcon } from "@/components/ui/icon/GoogleIcon";
+import { AppleIcon, GoogleIcon } from "@/components/ui/icon/GoogleIcon";
 import { signIn } from "next-auth/react";
 
-const SignInButton = () => {
+const SignInButton = ({ type }: { type: "google" | "kakao" }) => {
   const handleLogin = async () => {
-    // signIn 함수의 세 번째 파라미터로는 새 창에서 로그인하도록 설정할 수 있는 옵션이 있습니다.
-    // 여기서는 redirect: false로 설정하고, window.open을 사용하여 새 창에서 로그인 페이지를 엽니다.
-
-    await signIn("google", { callbackUrl: "/home", redirect: false });
+    // await signIn(type);
+    console.log(type);
+    if (type === "kakao") {
+      return await signIn("kakao", { callbackUrl: "/", redirect: false });
+    } else if (type === "google") {
+      return await signIn("google", { callbackUrl: "/home", redirect: false });
+    }
   };
+  if (type === "kakao") {
+    return (
+      <Button
+        onClick={handleLogin}
+        hoverColor="hoverLightBlack"
+        size="login1"
+        color="blue"
+        backgroundColor="white"
+        borderColor="gray"
+        title={
+          <div className="flex w-full h-full items-center justify-center gap-1 py-[1px] px-4">
+            <AppleIcon />
+            <span className="twitterSpan text-[15px] font-semibold">
+              Apple에서 가입하기
+            </span>
+          </div>
+        }
+      />
+    );
+  }
   return (
     <Button
       onClick={handleLogin}
