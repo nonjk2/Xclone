7f629b94f880a5851affcf7adce98079b3e9070f
최은석 <trgf456@naver.com>
2024-04-17 18:33:45 +0900

    [feat] 싱글포스트 서버사이드 쿼리

--- src/app/(isuser)/@modal/(.)[username]/status/[id]/photo/[photoid]/page.tsx
+++ src/app/(isuser)/@modal/(.)[username]/status/[id]/photo/[photoid]/page.tsx
@@ -1,18 +1,41 @@
+import { authOption } from "@/auth";
 import PhotoComponents from "@/components/photo/PhotoComponents";
 import Modal from "@/components/ui/modal";
 import KeyDownProvider from "@/context/KeyDownProvider";
+import { getSinglePost } from "@/lib/action/post-server";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getServerSession } from "next-auth";
 
 const Page = async ({
   params,
 }: {
   params: { username: string; id: string; photoid: number };
 }) => {
+  const session = await getServerSession(authOption);
+  const token = session?.supabaseAccessToken ?? "";
+  const { photoid, username, id } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["post", id],
+    queryFn: (key) =>
+      getSinglePost({
+        queryKey: key.queryKey as [string, string],
+        supabaseAccessToken: token,
+      }),
+  });
+  const state = dehydrate(queryClient);
   return (
-    <KeyDownProvider>
-      <Modal screen>
-        <PhotoComponents />
-      </Modal>
-    </KeyDownProvider>
+    <HydrationBoundary state={state}>
+      <KeyDownProvider>
+        <Modal screen>
+          <PhotoComponents id={id} />
+        </Modal>
+      </KeyDownProvider>
+    </HydrationBoundary>
   );
 };
 export default Page;
