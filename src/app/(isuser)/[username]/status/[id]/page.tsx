b764e27133f08a04030d760e174a3e68fdc2eaa1
최은석 <trgf456@naver.com>
2024-04-23 21:41:01 +0900

    [feat] 상세페이지 댓글수정

--- src/app/(isuser)/[username]/status/[id]/page.tsx
+++ src/app/(isuser)/[username]/status/[id]/page.tsx
@@ -1,43 +1,41 @@
-"use client";
-
 import Loading from "@/app/(isuser)/explore/loading";
-import MainCenterListItem from "@/components/main/center/home/HomePostItem";
+import { authOption } from "@/auth";
+import SinglePostComponent from "@/components/main/center/singlepost/SinglePostComponent";
 import PhotoBoardSection from "@/components/photo/PhotoBoardSection";
-import { getSinglePost } from "@/lib/action/post-server";
+
+import usePost from "@/lib/hooks/usePost";
+import { serverClient } from "@/lib/util/serverSBClient";
 import { supabaseClient } from "@/lib/util/supabase";
 
-import { useQuery } from "@tanstack/react-query";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+  useQuery,
+} from "@tanstack/react-query";
+import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
 interface SinglePostPageProps {
   params: { id: string };
 }
-const SiglePostPage = ({ params }: SinglePostPageProps) => {
-  const { id } = params;
-  const { data, status } = useSession();
-  if (status === "loading") {
-    <>loading...</>;
+const SiglePostPage = async ({ params }: SinglePostPageProps) => {
+  const { id: PostId } = params;
+  const session = await getServerSession(authOption);
+  const queryClient = new QueryClient();
+  if (!session?.supabaseAccessToken) {
+    return <>loding...</>;
   }
-  const client = supabaseClient(data?.supabaseAccessToken);
-  const { data: post, isPending } = useQuery<
-    Post,
-    Object,
-    Post,
-    [_1: string, string]
-  >({
-    queryKey: ["post", id],
-    queryFn: (queryKey) =>
-      getSinglePost({ queryKey: queryKey.queryKey, client }),
-  });
-  if (isPending) {
-    return <Loading />;
-  }
-  if (post) {
-    return (
-      <article className="w-full h-full">
-        <PhotoBoardSection Post={post} photo={false} />
-      </article>
-    );
-  }
-  return <>게시물이 존재하지않습니다.</>;
+
+  const client = serverClient(session?.supabaseAccessToken);
+  await queryClient.prefetchQuery<Post, Object, Post, [_1: string, string]>(
+    usePost({ client, PostId })
+  );
+  return (
+    <article className="w-full h-full">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SinglePostComponent PostId={PostId} />
+      </HydrationBoundary>
+    </article>
+  );
 };
 export default SiglePostPage;
